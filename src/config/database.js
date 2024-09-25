ca3caa86ad1d86bf7ba44ec7d0114868f0bb8dae
Bruno Luis <bruno.dev07@gmail.com>
2024-09-25 16:27:28 -0300

    Changes of link postgres

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: "postgres",
-  url: "postgresql://codeburger_postgres:postgres@codeburger_postgres:5432/codeburger",
+  url: "postgresql://codeburger_postgres:postgres@localhost:5432/codeburger",
   define: {
     timestamps: true,
     underscored: true,
